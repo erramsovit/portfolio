a4975090dc5851aec56db7ee098d41affb1d7691
erramsovit <er.ramsovit@gmail.com>
2021-01-09 15:44:03 +0545

    second commit

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .import views
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('home/', views.home , name='home'),
+    path('services/',views.services, name='services'),
+    path('skills/',views.skills, name='skills'),
+    path('contact/',views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'contact.html')
+    return render(request,'home.html',context={'home':'active'})
+
+def services(request):
+    return render(request,'services.html',context={'services':'active'})
+
+def skills(request):
+     return render(request,'skills.html',context={'skills':'active'})
+
+def contact(request):
+     return render(request,'contact.html',context={'contact':'active'})
+
+
+
